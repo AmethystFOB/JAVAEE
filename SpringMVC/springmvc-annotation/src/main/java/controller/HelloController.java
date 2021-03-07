b69d0f782f1d1295a3d7d41865a71891852ac7f2
alex_zjh <AmethystFOB@163.com>
2021-03-07 16:02:46 +0800

    SpringMVC-some changes

--- SpringMVC/springmvc-annotation/src/main/java/controller/HelloController.java
+++ SpringMVC/springmvc-annotation/src/main/java/controller/HelloController.java
@@ -5,6 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+//@RequestMapping("/prehello")    //若在类前加@RequestMapping，则访问地址变为localhost:8080/prehello/hello
 public class HelloController {
     @RequestMapping("/hello")
     public String Hello(Model model){
@@ -19,7 +20,7 @@ public class HelloController {
 //        model.addAttribute("msg","Hello SpringMVC Annotation!");
 //        return "hello";  //会被视图解析器处理
 //    }
-//
+
 //    @RequestMapping("/hello")
 //    public String Hello3(Model model){
 //        //封装数据
